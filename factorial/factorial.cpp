003994686a369f8eb338a1f8132b83a97ddfdb8e
DiegoAguirre <dfaguirre11@LAPTOP-5S4LD6RN.localdomain>
2020-05-21 00:42:52 -0500

    la funcion es perfecta

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -3,9 +3,24 @@ Tarea laboratorio metodos computacionales
 Hw08 Funciones en C++
  ***************************************************************/
 #include <iostream>
+using namespace std;
 
+int factorial(int i);
 int main(){
-
-
+  int n;
+  cout << "ingrece el número al que le quiere calcular el factorial"<< endl;
+  cin >> n;
+  int f = factorial(n);
+  cout << "n! = "<< f << endl;
   return 0;
 }
+
+ int factorial(int i){
+  if(i==0){
+    return 1;
+  }
+  else{
+    int b = i-1;
+    return i*factorial(b);
+  }
+		      }
